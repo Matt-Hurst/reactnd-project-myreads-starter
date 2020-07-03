28f18686464471226a0bc8ba3e055c6ebb58baa2
Matt Hurst <matt.adam.hurst@gmail.com>
2020-07-03 15:59:57 +0100

    add componentDidMount to get BooksAPI.getAll and pass state into BookCase page

--- src/App.js
+++ src/App.js
@@ -1,62 +1,44 @@
-import React, { useState } from 'react'
-// import * as BooksAPI from './BooksAPI'
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookCase from "./pages/BookCase"
 import Search from "./pages/Search"
 import { Route } from "react-router-dom"
 
-
 /* 
 TO DO:
-- Set up Reat Router
-
+- pass books into BookCase
+- filter books based on shelf - map
 */
+class BooksApp extends React.Component {
+  state = []
 
-const BookApp = () => {
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => {
+        this.setState((currentState) => (
+          {
+            books
+          }
+        )
+      )
+    })
+  }
 
+  render() {
+  // console.log(this.state)
 
-  return(
-    <div className="app">
-      <Route exact path='/' render={() => (
-        <BookCase />
-      )} />
-      <Route path='/search' component={Search} />
-    </div>
-  )
+    return(
+      <div className="app">
+        <Route exact path='/' render={() => (
+          <BookCase 
+            books = {this.state}
+          />
+        )} />
+        <Route path='/search' component={Search} />
+      </div>
+    )
+  }
 }
 
-export default BookApp
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class BooksApp extends React.Component {
-//   state = {
-
-//     showSearchPage: false
-//   }
-
-//   render() {
-//     return (
-//       <div className="app">
-        
-         
-//         )}
-//       </div>
-//     )
-//   }
-// }
-
-// export default BooksApp
+export default BooksApp
